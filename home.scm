36fbd76d542865645966fe2801d63bffe70536b1
agx <agxr@tuta.io>
2025-05-23 20:03:58 +0500

    Refactored home.scm, fixed foot's TERMINAL var

--- home.scm
+++ home.scm
@@ -1,122 +1,134 @@
-(use-modules (guix utils)
-             (guix gexp)
-             (gnu home)
-             (gnu home services)
-             (gnu packages)
-             (gnu services))
+(use-modules
+ (gnu home)
+ (gnu home services)
+ (gnu packages)
+ (gnu services)
+ (guix gexp)
+ (guix utils))
 
+;; Define base path for dotfiles
+(define home-dir (getenv "HOME"))
+(define dotfiles-dir (string-append home-dir "/guix-home/dotfiles"))
+(define modules-dir (string-append home-dir "/guix-home/modules"))
+(define encrypted-dir (string-append home-dir "/guix-home/encrypted"))
+
+;; Load package and Emacs configurations
 (define home-package-list
-  (load (string-append (getenv "HOME") "/guix-home/modules/package-list.scm")))
+  (load (string-append modules-dir "/package-list.scm")))
+(load (string-append modules-dir "/emacs.scm"))
 
-(load (string-append (getenv "HOME") "/guix-home/modules/emacs.scm"))
+(define (dotfile path)
+  "Helper to create a local-file from dotfiles directory."
+  (local-file (string-append dotfiles-dir "/" path)))
+
+(define (recursive-dotfile path)
+  "Helper for recursive local-file from dotfiles directory."
+  (local-file (string-append dotfiles-dir "/" path) #:recursive? #t))
+
+(define (script-file name source-path)
+  "Helper to create executable script files."
+  (computed-file
+   name
+   #~(begin
+       (copy-file #$(dotfile source-path) #$output)
+       (chmod #$output #o755))))
 
 (home-environment
  (packages
   (append home-package-list home-emacs-packages))
  (services
   (list
-   (simple-service 'env-vars-service
-                   home-environment-variables-service-type
-                   '(("TERM" . "xterm-256color")
-                     ("PAGER" . "less")
-		     ("LESS" . "-iMR")
-                     ("COLORTERM" . "truecolor")
-                     ("MOZ_ENABLE_WAYLAND" . "1")
-                     ("XDG_SESSION_TYPE" . "wayland")
-                     ("VISUAL" . "emacsclient")
-		     ("EDITOR" . "emacsclient")
-		     ("BAT_THEME" . "mocha")
-		     ("BAT_OPTS" . "-Pp")
-		     ("XDG_DOWNLOAD_DIR" . "${HOME}/downloads")
-		     ("PATH" . "${HOME}/.local/bin:${HOME}/bin${PATH:+:$PATH}")
-                     ("XDG_SESSION_DESKTOP" . "sway")
-                     ("XDG_CURRENT_DESKTOP" . "sway")))
-   (simple-service 'dotfiles-service
-                   home-files-service-type
-                   `((".zshrc"                      ,(local-file (string-append (getenv "HOME") "/guix-home/dotfiles/zsh/zshrc")))
-                     (".zshrc_foot"                 ,(local-file (string-append (getenv "HOME") "/guix-home/dotfiles/zsh/zshrc_foot")))
-                     (".zshrc_tty"                  ,(local-file (string-append (getenv "HOME") "/guix-home/dotfiles/zsh/zshrc_tty")))
-
-                     (".config/zathura/zathurarc"         ,(local-file (string-append (getenv "HOME") "/guix-home/dotfiles/zathura/zathurarc")))
-                     (".config/zathura/catppuccin-mocha"  ,(local-file (string-append (getenv "HOME") "/guix-home/dotfiles/zathura/catppuccin-mocha")))
-
-                     (".config/sway/config"         ,(local-file (string-append (getenv "HOME") "/guix-home/dotfiles/sway/config")))
-                     (".config/sway/colors"         ,(local-file (string-append (getenv "HOME") "/guix-home/dotfiles/sway/colors")))
-                     (".config/sway/wallpapers.png" ,(local-file (string-append (getenv "HOME") "/guix-home/dotfiles/sway/wallpapers.png")))
-
-                     (".config/qutebrowser/config.py"              ,(local-file (string-append (getenv "HOME") "/guix-home/dotfiles/qutebrowser/config.py")))
-                     (".config/qutebrowser/start.html"             ,(local-file (string-append (getenv "HOME") "/guix-home/dotfiles/qutebrowser/start.html")))
-                     (".config/qutebrowser/startpic.jpg"           ,(local-file (string-append (getenv "HOME") "/guix-home/dotfiles/qutebrowser/startpic.jpg")))
-                     (".config/qutebrowser/catppuccin/setup.py"    ,(local-file (string-append (getenv "HOME") "/guix-home/dotfiles/qutebrowser/catppuccin/setup.py")))
-                     (".config/qutebrowser/catppuccin/__init__.py" ,(local-file (string-append (getenv "HOME") "/guix-home/dotfiles/qutebrowser/catppuccin/__init__.py")))
-
-		     (".encrypted"
-		      ,(local-file
-			(string-append
-			 (getenv "HOME")
-			 "/guix-home/encrypted")
-			#:recursive? #t))
-
-		     (".icons/default"
-		      ,(local-file
-			(string-append
-			 (getenv "HOME")
-			 "/guix-home/dotfiles/cursors")
-			#:recursive? #t))
-
-                     (".emacs.d/init.el" ,(local-file (string-append (getenv "HOME") "/guix-home/dotfiles/emacs/init.el")))
-		     (".emacs.d/lisp"
-		      ,(local-file
-			(string-append
-			 (getenv "HOME")
-			 "/guix-home/dotfiles/emacs/lisp")
-			#:recursive? #t))
-
-                     (".config/mako/config"         ,(local-file (string-append (getenv "HOME") "/guix-home/dotfiles/mako/config")))
-
-                     (".config/fuzzel/fuzzel.ini"   ,(local-file (string-append (getenv "HOME") "/guix-home/dotfiles/fuzzel/fuzzel.ini")))
-                     (".config/fuzzel/launch-list" ,(local-file (string-append (getenv "HOME") "/guix-home/dotfiles/fuzzel/launch-list")))
-		     (".local/bin/menu.sh"
-			,(computed-file "menu.sh"
-					#~(begin
-					    (copy-file
-					    #$(local-file (string-append (getenv "HOME") "/guix-home/dotfiles/fuzzel/menu.sh"))
-					    #$output)
-					    (chmod #$output #o755))))
-		     (".local/bin/ac-on.sh"
-			,(computed-file "ac-on.sh"
-					#~(begin
-					    (copy-file
-					    #$(local-file (string-append (getenv "HOME") "/guix-home/dotfiles/fuzzel/ac-on.sh"))
-					    #$output)
-					    (chmod #$output #o755))))
-		     (".local/bin/ac-off.sh"
-			,(computed-file "ac-off.sh"
-					#~(begin
-					    (copy-file
-					    #$(local-file (string-append (getenv "HOME") "/guix-home/dotfiles/fuzzel/ac-off.sh"))
-					    #$output)
-					    (chmod #$output #o755))))
-
-                     (".config/waybar/config"       ,(local-file (string-append (getenv "HOME") "/guix-home/dotfiles/waybar/config")))
-                     (".config/waybar/style.css"    ,(local-file (string-append (getenv "HOME") "/guix-home/dotfiles/waybar/style.css")))
-
-                     (".config/tmux/tmux.conf"      ,(local-file (string-append (getenv "HOME") "/guix-home/dotfiles/tmux/tmux.conf")))
-
-                     (".config/starship.toml"       ,(local-file (string-append (getenv "HOME") "/guix-home/dotfiles/starship/starship.toml")))
-
-		     (".config/bat/themes/mocha.tmTheme"
-		      ,(local-file (string-append (getenv "HOME") "/guix-home/dotfiles/bat/mocha.tmTheme")))
-
-		     (".config/fastfetch/config.jsonc"
-		      ,(local-file (string-append (getenv "HOME") "/guix-home/dotfiles/fastfetch/config.jsonc")))
-		     (".config/fastfetch/logo.png"
-		      ,(local-file (string-append (getenv "HOME") "/guix-home/dotfiles/fastfetch/logo.png")))
-
-		     (".config/foot/foot.ini"       ,(local-file (string-append (getenv "HOME") "/guix-home/dotfiles/foot/foot.ini")))
-
-		     (".config/helix/config.toml"   ,(local-file (string-append (getenv "HOME") "/guix-home/dotfiles/helix/config.toml")))
-
-		     (".lesskey"   ,(local-file (string-append (getenv "HOME") "/guix-home/dotfiles/less/lesskey")))
-
-		     (".config/ranger/rc.conf"   ,(local-file (string-append (getenv "HOME") "/guix-home/dotfiles/ranger/rc.conf"))))))))
+   ;; Environment variables
+   (simple-service
+    'env-vars-service
+    home-environment-variables-service-type
+    '(("TERM" . "xterm-256color")
+      ("PAGER" . "less")
+      ("LESS" . "-iMR")
+      ("COLORTERM" . "truecolor")
+      ("MOZ_ENABLE_WAYLAND" . "1")
+      ("XDG_SESSION_TYPE" . "wayland")
+      ("VISUAL" . "emacsclient")
+      ("EDITOR" . "emacsclient")
+      ("BAT_THEME" . "mocha")
+      ("BAT_OPTS" . "-Pp")
+      ("XDG_DOWNLOAD_DIR" . "${HOME}/downloads")
+      ("PATH" . "${HOME}/.local/bin:${HOME}/bin${PATH:+:$PATH}")
+      ("XDG_SESSION_DESKTOP" . "sway")
+      ("XDG_CURRENT_DESKTOP" . "sway")))
+   
+   ;; Dotfiles
+   (simple-service
+    'dotfiles-service
+    home-files-service-type
+    `(;; Zsh configuration
+      (".zshrc" ,(dotfile "zsh/zshrc"))
+      (".zshrc_foot" ,(dotfile "zsh/zshrc_foot"))
+      (".zshrc_tty" ,(dotfile "zsh/zshrc_tty"))
+      
+      ;; Zathura configuration
+      (".config/zathura/zathurarc" ,(dotfile "zathura/zathurarc"))
+      (".config/zathura/catppuccin-mocha" ,(dotfile "zathura/catppuccin-mocha"))
+      
+      ;; Sway configuration
+      (".config/sway/config" ,(dotfile "sway/config"))
+      (".config/sway/colors" ,(dotfile "sway/colors"))
+      (".config/sway/wallpapers.png" ,(dotfile "sway/wallpapers.png"))
+      
+      ;; Qutebrowser configuration
+      (".config/qutebrowser/config.py" ,(dotfile "qutebrowser/config.py"))
+      (".config/qutebrowser/start.html" ,(dotfile "qutebrowser/start.html"))
+      (".config/qutebrowser/startpic.jpg" ,(dotfile "qutebrowser/startpic.jpg"))
+      (".config/qutebrowser/catppuccin/setup.py" ,(dotfile "qutebrowser/catppuccin/setup.py"))
+      (".config/qutebrowser/catppuccin/__init__.py" ,(dotfile "qutebrowser/catppuccin/__init__.py"))
+      
+      ;; Encrypted directory
+      (".encrypted" ,(local-file encrypted-dir #:recursive? #t))
+      
+      ;; Cursor icons
+      (".icons/default" ,(recursive-dotfile "cursors"))
+      
+      ;; Emacs configuration
+      (".emacs.d/init.el" ,(dotfile "emacs/init.el"))
+      (".emacs.d/lisp" ,(recursive-dotfile "emacs/lisp"))
+      
+      ;; Mako notification daemon
+      (".config/mako/config" ,(dotfile "mako/config"))
+      
+      ;; Fuzzel launcher
+      (".config/fuzzel/fuzzel.ini" ,(dotfile "fuzzel/fuzzel.ini"))
+      (".config/fuzzel/launch-list" ,(dotfile "fuzzel/launch-list"))
+      
+      ;; Executable scripts
+      (".local/bin/menu.sh" ,(script-file "menu.sh" "fuzzel/menu.sh"))
+      (".local/bin/ac-on.sh" ,(script-file "ac-on.sh" "fuzzel/ac-on.sh"))
+      (".local/bin/ac-off.sh" ,(script-file "ac-off.sh" "fuzzel/ac-off.sh"))
+      
+      ;; Waybar configuration
+      (".config/waybar/config" ,(dotfile "waybar/config"))
+      (".config/waybar/style.css" ,(dotfile "waybar/style.css"))
+      
+      ;; Tmux configuration
+      (".config/tmux/tmux.conf" ,(dotfile "tmux/tmux.conf"))
+      
+      ;; Starship prompt
+      (".config/starship.toml" ,(dotfile "starship/starship.toml"))
+      
+      ;; Bat theme
+      (".config/bat/themes/mocha.tmTheme" ,(dotfile "bat/mocha.tmTheme"))
+      
+      ;; Fastfetch configuration
+      (".config/fastfetch/config.jsonc" ,(dotfile "fastfetch/config.jsonc"))
+      (".config/fastfetch/logo.png" ,(dotfile "fastfetch/logo.png"))
+      
+      ;; Foot terminal
+      (".config/foot/foot.ini" ,(dotfile "foot/foot.ini"))
+      
+      ;; Helix editor
+      (".config/helix/config.toml" ,(dotfile "helix/config.toml"))
+      
+      ;; Less key bindings
+      (".lesskey" ,(dotfile "less/lesskey"))
+      
+      ;; Ranger file manager
+      (".config/ranger/rc.conf" ,(dotfile "ranger/rc.conf")))))))
