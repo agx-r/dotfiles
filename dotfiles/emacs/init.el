de101ebc3efef45bef82b4d8d2594b7e91928dd5
agx <agxr@tuta.io>
2025-06-09 03:45:40 +0500

    Small improvements

--- dotfiles/emacs/init.el
+++ dotfiles/emacs/init.el
@@ -1,4 +1,9 @@
-;; -*- lexical-binding: t; -*-
+;;; package --- Init -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;; Inits configs
+
+;;; Code:
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
 (require 'core-config)
@@ -9,3 +14,4 @@
 (require 'completion-config)
 (require 'dev-config)
 (require 'scheme-config)
+;;; init.el ends here
