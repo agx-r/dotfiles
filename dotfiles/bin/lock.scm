1a7c29a7aab418234c9cb21230f1646c1c103ee3
agx <agx@localhost.my.domain>
2025-10-26 03:13:35 +0500

    swaylock updated

---
+++ dotfiles/bin/lock.scm
@@ -0,0 +1,3 @@
+#!/usr/bin/env guile
+!#
+(system "swaylock --screenshots --indicator-radius 100 --indicator-thickness 17 --clock --fade-in 2 -e --indicator --font 41 --effect-greyscale --effect-pixelate 15")
