842f60ca3e906382b2858b072d638f2a2fe725ad
agx <agxr@genocide.fun>
2025-12-03 13:27:39 +0500

    Aliases updated

--- dotfiles/config/powershell/Microsoft.PowerShell_profile.ps1
+++ dotfiles/config/powershell/Microsoft.PowerShell_profile.ps1
@@ -4,11 +4,16 @@ Set-Alias l "eza"
 Set-Alias n "cd ~/.config/home-manager"
 Set-Alias h "hx"
 Set-Alias b "cd .."
+Set-Alias x "nix"
+Set-Alias xs "home-manager switch"
+Set-Alias xb "nix build"
+Set-Alias xv "nixVulkanIntel"
+Set-Alias xg "nixGLIntel"
 
 Set-Alias g "git"
+Set-Alias ga "git add"
 Set-Alias gs "git status"
 Set-Alias gc "git commit"
-Set-Alias ga "git add"
 
 function Prompt {
     $path = (Get-Location).Path
